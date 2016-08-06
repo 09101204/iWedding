e4ae9f947794751aad9dbe33cdfd9ab2e05d9b06
laznrbfe <laznrbfe@163.com>
2016-08-07 00:59:44 +0800

    LoginNoticeView

---
+++ iWedding/iWedding/General/Views/LoginNoticeView/LoginNoticeView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginNoticeView.swift
+//  iWedding
+//
+//  Created by 张青明 on 16/8/7.
+//  Copyright © 2016年 极客栈. All rights reserved.
+//
+
+import UIKit
+
+class LoginNoticeView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
