8d4737c6cb144f3c61e704e19de0dc73f91a5d7a
laznrbfe <laznrbfe@163.com>
2016-08-07 00:22:04 +0800

    UI入口

---
+++ iWedding/iWedding/Sections/Main(UI入口)/MainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewController.swift
+//  iWedding
+//
+//  Created by 张青明 on 16/8/6.
+//  Copyright © 2016年 极客栈. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: NSObject {
+
+}
