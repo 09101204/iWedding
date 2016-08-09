c0e1533aaddd5bfa8f42524e4d40fa61d07d6f57
laznrbfe <laznrbfe@163.com>
2016-08-09 23:40:48 +0800

    自定义Navbar

---
+++ iWedding/iWedding/General/Views/CommunityPersonNavbar/CommunityPersonNavbar.swift
@@ -0,0 +1,38 @@
+//
+//  CommunityPersonNavbar.swift
+//  iWedding
+//
+//  Created by 张青明 on 16/8/9.
+//  Copyright © 2016年 极客栈. All rights reserved.
+//
+
+import UIKit
+
+class CommunityPersonNavbar: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    // MARK: - 唤醒view
+    override func awakeFromNib() {
+        
+        self.autoresizingMask = UIViewAutoresizing.None
+    }
+    
+    func navbar() -> CommunityPersonNavbar {
+        let navbar = NSBundle.mainBundle().loadNibNamed("CommunityPersonNavbar", owner: nil, options: nil).first as! CommunityPersonNavbar
+        
+        let width = UIScreen.mainScreen().bounds.size.width
+        
+        navbar.frame = CGRectMake(0, 0, width, 64.0)
+        
+        return navbar
+        
+    }
+
+}
