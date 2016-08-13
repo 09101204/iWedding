1c6d49418009b10e825d8f04c3b8b6755e64f3a7
laznrbfe <laznrbfe@163.com>
2016-08-13 23:42:53 +0800

    “我的”cell

---
+++ iWedding/iWedding/Sections/Mine(我的)/View/UserCenterNormalCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserCenterNormalCell.swift
+//  iWedding
+//
+//  Created by 张青明 on 16/8/13.
+//  Copyright © 2016年 极客栈. All rights reserved.
+//
+
+import UIKit
+
+class UserCenterNormalCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
